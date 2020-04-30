040380978dc95e3423c503d5f04b88d7f1aa91ff
mgaikwad <mayurigaikwad797@gmail.com>
2020-04-30 14:38:02 +0300

    modified script

--- run_cp.sh
+++ run_cp.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-cp -rf /var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_NUMBER/archive/$BUILD_NUMBER.zip /home/mgaikwad/Desktop/log/
+cp -rf /var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_NUMBER/archive/$BUILD_NUMBER.zip /var/lib/jenkins/workspace/$JOB_NAME/src/
+
